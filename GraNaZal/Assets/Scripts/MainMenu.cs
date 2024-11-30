d1b6887fda90bccc6eab715eb781833948262c58
166236uwm <92304880+166236uwm@users.noreply.github.com>
2024-11-30 01:05:00 +0100

    mainmenu finished

--- GraNaZal/Assets/Scripts/MainMenu.cs
+++ GraNaZal/Assets/Scripts/MainMenu.cs
@@ -3,5 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    public void PlayGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
